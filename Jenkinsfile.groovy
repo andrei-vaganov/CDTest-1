9f61e0034f56ff5b4ab38b1d910aa14d8db43dfc
Andrei Vaganov <guentomuento@gmail.com>
2021-09-28 02:22:42 +0300

    test

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,31 @@
+pipeline {
+    agent any
+
+    environment {
+
+        CRD = credentials('i1-root')
+
+    }
+
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Hello World'
+                echo 'How are you?'
+            }
+        }
+        
+        stage('GGG') {
+            steps {
+                echo "!!!! ${BRANCH_NAME}"
+                echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}, ${env.BRANCH_NAME}, ${env.CHANGE_AUTHOR}, ${env.CHANGE_TARGET}, ${env.CHANGE_BRANCH},"
+            }
+        }
+        stage('Deployyyy') {
+            steps {
+                echo 'mu mu mu!'
+            }
+        }
+
+    }
+}
