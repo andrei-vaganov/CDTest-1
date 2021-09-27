4a629567373121fc0ce8c9cb2e04da8870fe5012
Andrei Vaganov <guentomuento@gmail.com>
2021-09-28 02:28:50 +0300

    test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ pipeline {
     environment {
 
         CRD = credentials('i1-root')
-        BRANCH_NAME = sh(label: 'getBranchName', returnStdout: true, script: 'git rev-parse --abbrev-ref HEAD').trim()
+        BRANCH_NAME = scm.branches[0].name
             
 
     }
@@ -14,6 +14,7 @@ pipeline {
             steps {
                 echo 'Hello World'
                 echo 'How are you?'
+                sh 'printenv'
             }
         }
         
